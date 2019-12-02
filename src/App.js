272d79a714bd4df0b020d15dd6c197e626c70337
Muhammad Hakimi <kimimaula@gmail.com>
2019-12-02 15:23:05 +0800

    beginningv115

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './css/App.css';
+
+class Tutorial extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1> Hello Kimi </h1>
+      </div>
+    );
+  }
+}
+
+export default Tutorial;
